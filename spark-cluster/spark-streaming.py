89d6d721f98cea59fc14171b0f0fdcfa0314e572
noooey <20203065@kookmin.ac.kr>
2023-04-04 18:15:27 +0900

    :sparkles: not print info

--- spark-cluster/spark-streaming.py
+++ spark-cluster/spark-streaming.py
@@ -1,37 +1,50 @@
 import findspark
 findspark.init()
 
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from pyspark.sql import *
+# from pyspark.sql.functions import *
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, LongType
+# from pyspark.sql import *
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
-from pyspark.sql.functions import col, pandas_udf, split
+from pyspark.sql.functions import udf, col, pandas_udf, split
 
-#sparkSession 생성
-sc = SparkSession \
+KAFKA_BOOTSTRAP_SERVERS = "broker-1:29092, broker-2:29093, broker-3:29094"
+KAFKA_TOPIC = "youtube_comments"
+
+# Create Schema
+# SCHEMA = StructType([
+#     StructField("ID", LongType()),             # COMMENT ID
+#     StructField("DATETIME", TimestampType()),  # Date and hour
+#     StructField("USER_ID", LongType()),        # UNIQUE USER ID
+#     StructField("USER", StringType()),         # USER NAME
+#     StructField("CONTENT", StringType()),      # CONTENT
+# ])
+
+# Create a Spark Session
+spark = SparkSession \
     .builder \
     .appName("kafka-streaming") \
     .master("spark://spark-master:7077") \
     .getOrCreate()
 
-# spark structured streaming을 이용
-# kafka에서 데이터를 읽어와 dataframe 생성
-df = sc \
+spark.sparkContext.setLogLevel('WARN')
+
+# Using Spark Structed Streaming
+# Read messages from Kafka Topic and Create Dataframe
+df = spark \
     .readStream\
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "broker-1:29092, broker-2:29093, broker-3:29094") \
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS) \
     .option("failOnDataLoss","False") \
-    .option("subscribe", "youtube_comments") \
+    .option("subscribe", KAFKA_TOPIC) \
     .option("startingOffsets", "earliest") \
     .load()
 
-df.awaitTermination()
-# 
-# # Write stream - console
-# query = df.selectExpr("CAST(value AS STRING)") \
-# .writeStream \
-# .format("console") \
-# .option("truncate", "false") \
-# .start()
-# .awaitTermination()
+# df.awaitTermination()
+
+# Write stream - console
+df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+    .writeStream \
+    .format("console") \
+    .outputMode("append") \
+    .start() \
+    .awaitTermination()
