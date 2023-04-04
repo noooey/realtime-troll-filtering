467c70fbda89cdbec6367f663ea20c66e95efb05
noooey <20203065@kookmin.ac.kr>
2023-04-04 19:04:53 +0900

    :sparkles: add schema kafka 토픽에서 데이터를 받아올 때, key:value 형태가 아닌, 데이터 프레임 형태로 받아올 수 있도록 스키마 추가

--- spark-cluster/spark-streaming.py
+++ spark-cluster/spark-streaming.py
@@ -5,19 +5,18 @@ findspark.init()
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, LongType
 # from pyspark.sql import *
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col, pandas_udf, split
+from pyspark.sql.functions import udf, col, pandas_udf, split, from_json, expr
 
 KAFKA_BOOTSTRAP_SERVERS = "broker-1:29092, broker-2:29093, broker-3:29094"
 KAFKA_TOPIC = "youtube_comments"
 
 # Create Schema
-# SCHEMA = StructType([
-#     StructField("ID", LongType()),             # COMMENT ID
-#     StructField("DATETIME", TimestampType()),  # Date and hour
-#     StructField("USER_ID", LongType()),        # UNIQUE USER ID
-#     StructField("USER", StringType()),         # USER NAME
-#     StructField("CONTENT", StringType()),      # CONTENT
-# ])
+SCHEMA = StructType([
+    StructField("id", StringType(), True),           # COMMENT ID
+    StructField("datetime", TimestampType(), True),  # Date and hour
+    StructField("user", StringType(), True),         # USER NAME
+    StructField("content", StringType(), True),      # CONTENT
+])
 
 # Create a Spark Session
 spark = SparkSession \
@@ -42,7 +41,14 @@ df = spark \
 # df.awaitTermination()
 
 # Write stream - console
-df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+# df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+#     .writeStream \
+#     .format("console") \
+#     .outputMode("append") \
+#     .start() \
+#     .awaitTermination()
+df.select(from_json(col("value").cast("string"), SCHEMA).alias("value")) \
+    .selectExpr('value.id', 'value.datetime', 'value.user', 'value.content') \
     .writeStream \
     .format("console") \
     .outputMode("append") \
