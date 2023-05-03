94057cbe63b916b2e7ba2681e66ef01fef6e2cae
noooey <20203065@kookmin.ac.kr>
2023-05-03 20:00:06 +0900

    :poop: have to fix..

--- fast-api/inference.py
+++ fast-api/inference.py
@@ -1,35 +1,54 @@
-from pydantic import BaseModel
-# from transformers import KoBertModel
 from transformers import BertModel
+from fastapi import FastAPI
 import torch
-from fastapi import FastAPI, HTTPException
+import numpy as np
+from kobert_tokenizer import KoBERTTokenizer
+from schema import InputSchema, OutputSchema
 
-# Define InputSchema using Pydantic
-class InputSchema(BaseModel):
-    input_ids: list
-    attention_mask: list
-
-# Load the saved KoBERT model
-model_path = 'saved_model.pt'
-# model = KoBertModel.from_pretrained(model_path)
-model = BertModel.from_pretrained("monologg/kobert")
+tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')  
+model = BertModel.from_pretrained("Haaaaeun/kobert_hatespeech")
+max_seq_length=64
 
 # Create FastAPI instance
 app = FastAPI()
 
 # Define the prediction endpoint
-@app.post("/predict")
-async def predict(input_data: InputSchema):
-    # Convert input data to torch tensors
-    input_ids = torch.tensor(input_data.input_ids)
-    attention_mask = torch.tensor(input_data.attention_mask)
-
-    # Forward pass through the KoBERT model
+@app.post("/predict", response_model=OutputSchema)
+def get_inference(input_data: InputSchema) -> OutputSchema:
+    inputs = tokenizer(
+        [input_data.sentence],
+        max_length=max_seq_length,
+        padding="max_length",
+        truncation=True,
+    )
     with torch.no_grad():
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-        inferred_value = outputs.pooler_output
+        sequence_output, pooled_output = model(**{k: torch.tensor(v) for k, v in inputs.items()})
+        print(sequence_output)
+        test_eval=[]
+        # for i in out:
+        logits=sequence_output
+        probs = logits.sigmoid()
+        print('probs!!!!')
+        print(probs)
+        preds = (probs > 0.5).long()
+        print('preds!!!!')
+        print(preds)
+        preds = preds.detach().cpu().numpy()
+        print(preds.shape)
 
-    return {'inferred_value': inferred_value.tolist()}
+        if np.argmax(logits) >= 0.5:
+            test_eval.append("부정적")
+        else:
+            test_eval.append("긍정적")
+    print('**********')
+    print(test_eval)
+    print('**********')
+    return OutputSchema(sentence=input_data.sentence, result=test_eval[0])
+    # {
+    #     "sentence": input_data.sentence,
+    #     "result": test_eval[0],
+    # }
+
 
 if __name__ == "__main__":
     # Run FastAPI locally with uvicorn server
