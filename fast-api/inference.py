b091ecfa6eb782f0a29e20d93086fac5bf0b9a85
noooey <20203065@kookmin.ac.kr>
2023-05-30 18:40:50 +0900

    :white_check_mark: KoELECTRA FastAPI Local Test

--- fast-api/inference.py
+++ fast-api/inference.py
@@ -6,16 +6,28 @@ import requests
 requests.get('https://www.huggingface.co')
 from schema import InputSchema, OutputSchema
 
+'''
+model_name = ["Haaaaeun/kobert_hatespeech",
+              "Haaaaeun/kcbert_hatespeech",
+              "Haaaaeun/koELECTRA_hatespeech"]
+'''
+
 ############# KoBERT #############
-from kobert_tokenizer import KoBERTTokenizer
-tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
-model = BertModel.from_pretrained("Haaaaeun/kobert_hatespeech")
+# from kobert_tokenizer import KoBERTTokenizer
+# tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+# model = BertModel.from_pretrained("Haaaaeun/kobert_hatespeech")
 ####################################
 
 ########### SoonsilBERT ###########
 # tokenizer = AutoTokenizer.from_pretrained("Haaaaeun/kcbert_hatespeech")
 # model = BertModel.from_pretrained("Haaaaeun/kcbert_hatespeech")
 ####################################
+
+########## KoELECTRA ###########
+from transformers import AutoModelForSequenceClassification
+tokenizer = AutoTokenizer.from_pretrained("Haaaaeun/koELECTRA_hatespeech")
+model = AutoModelForSequenceClassification.from_pretrained("Haaaaeun/koELECTRA_hatespeech")
+################################
 
 max_seq_length=64
 
@@ -33,29 +45,30 @@ app = FastAPI()
 @app.post("/predict", response_model=OutputSchema)
 def get_inference(input_data: InputSchema) -> OutputSchema:
     ############# KoBERT #############
-    inputs = tokenizer(
-        [input_data.sentence],
-        max_length=max_seq_length,
-        padding="max_length",
-        truncation=True,
-    )
-    ####################################
-
-    ########### SoonsilBERT ###########
-    # inputs = tokenizer.encode_plus(
-    #     input_data.sentence,
-    #     add_special_tokens=True,
-    #     return_attention_mask=True,
-    #     return_tensors="pt"
+    # inputs = tokenizer(
+    #     [input_data.sentence],
+    #     max_length=max_seq_length,
+    #     padding="max_length",
+    #     truncation=True,
     # )
     ####################################
 
+    ########### SoonsilBERT ###########
+    inputs = tokenizer.encode_plus(
+        input_data.sentence,
+        add_special_tokens=True,
+        return_attention_mask=True,
+        return_tensors="pt"
+    )
+    ####################################
+
+    model.eval()
     with torch.no_grad():
         ############# KoBERT #############
-        sequence_output, pooled_output = model(**{k: torch.tensor(v) for k, v in inputs.items()})
+        # sequence_output, pooled_output = model(**{k: torch.tensor(v) for k, v in inputs.items()})
 
-        # Pass the pooled_output through the classifier to obtain the classification result
-        classification_result = classifier(pooled_output)
+        # # Pass the pooled_output through the classifier to obtain the classification result
+        # classification_result = classifier(pooled_output)
         ####################################
 
         ########### SoonsilBERT ###########
@@ -68,8 +81,15 @@ def get_inference(input_data: InputSchema) -> OutputSchema:
         # classification_result = classifier(outputs)
         ####################################
 
+        ############## KoELECTRA ##############
+        outputs = model(**inputs)
+        logits = outputs.logits
+        probabilities = torch.softmax(logits, dim=1)
+        predicted_class = torch.argmax(probabilities, dim=1).item()
+        ##############################################################
+
         # Obtain the predicted class
-        predicted_class = torch.round(classification_result).item()
+        # predicted_class = torch.round(classification_result).item()
 
         # Print the predicted class
         if predicted_class == 1:
@@ -80,7 +100,7 @@ def get_inference(input_data: InputSchema) -> OutputSchema:
     print(OutputSchema(id=input_data.id, sentence=input_data.sentence, result=predicted_class))
     return OutputSchema(id=input_data.id, sentence=input_data.sentence, result=predicted_class)
 
-# if __name__ == "__main__":
-#     # Run FastAPI locally with uvicorn server
-#     import uvicorn
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
+if __name__ == "__main__":
+    # Run FastAPI locally with uvicorn server
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
